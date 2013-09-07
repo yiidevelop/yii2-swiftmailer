6c36e760f9ca0e43e1cc30f0171474df7e448f7f
Nghia Nguyen <zinzinday@gmail.com>
2013-09-08 02:49:18 +0700

    [WIP] Mailer

--- yii/mailer/Mailer.php
+++ yii/mailer/Mailer.php
@@ -13,22 +13,39 @@ namespace yii\mailer;
 use Yii;
 
 /**
+ * The component mailer for yii2 framework to send email message.
  * 
- * @property \Swift_Mailer $mailer Description
- * @property \Swift_Message $message Description
- * @property \Swift_Transport $transport Description
+ * @property \Swift_Mailer $mailer Swift Mailer class.
+ * @property \Swift_Message $message Message (RFC 2822) object.
+ * @property \Swift_Transport $transport The Transport used to send messages.
  */
 class Mailer extends \yii\base\Object
 {
 
+    /**
+     * Set the priority of this message.
+     * @var int The value is an integer where 1 is the highest priority and 5 is the lowest. 
+     */
     public $priority = 3;
+
+    /**
+     * @var string The character set of this message.
+     */
     public $charset = 'utf-8';
+
+    /**
+     * @var string The Content-type of this message.
+     */
     public $contentType = 'text/html';
+
+    /**
+     * @var string The Transport used to send messages. cases smtp|sendmail default is php.
+     */
     public $transportType = 'php';
     public $transportOptions = array();
 
     /**
-     * @var string|array
+     * @var string|array The sender of this message.
      */
     public $from = array('webmaster@localhost', 'Webmaster');
 
@@ -37,10 +54,26 @@ class Mailer extends \yii\base\Object
      */
     public $failedRecipients = array();
     public $viewPath = '@app/views/mailer';
+
+    /**
+     * The Transport used to send messages 
+     */
     protected $_transport;
+
+    /**
+     * The Message (RFC 2822) object.
+     */
     protected $_message;
+
+    /**
+     * Swift Mailer class.
+     */
     protected $_mailer;
 
+    /**
+     * Swift Mailer class.
+     * @return \Swift_Mailer
+     */
     public function getMailer()
     {
         if (empty($this->_mailer)) {
@@ -49,6 +82,10 @@ class Mailer extends \yii\base\Object
         return $this->_mailer;
     }
 
+    /**
+     * A Message (RFC 2822) object.
+     * @return \Swift_Message
+     */
     public function getMessage()
     {
         if (empty($this->_message)) {
@@ -61,6 +98,10 @@ class Mailer extends \yii\base\Object
         return $this->_message;
     }
 
+    /**
+     * The Transport used to send messages.
+     * @return \Swift_Transport
+     */
     public function getTransport()
     {
         if (!empty($this->_transport)) {
@@ -91,6 +132,13 @@ class Mailer extends \yii\base\Object
         return $this->_transport;
     }
 
+    /**
+     * Attach a [[Swift_Mime_MimeEntity]] such as an Attachment or MimePart.
+     * @param string $path Path a file to attach. (ex: Yii::getAlias('@webroot/upload/docs.pdf'); ).
+     * @param string $filename The visual file name for attachment file. (ex: document.pdf).
+     * @param string $contentType The header content type of file. (ex: application/pdf).
+     * @return type
+     */
     public function attachment($path, $filename = null, $contentType = null)
     {
         $attachment = \Swift_Attachment::fromPath($path, $contentType);
@@ -114,30 +162,49 @@ class Mailer extends \yii\base\Object
 
     /**
      * Add a To: address to this message.
-     * If $name is passed this name will be associated with the address.
-     * @param string|array $address
+     * ```php
+     *      $mailer->setTo('name@domain'=>'Your Name');
+     * ```
+     * @param string|array $addresses
      * @return \Swift_Message
      */
     public function setTo($addresses)
     {
-        return $this->message->setTo($addresses);
+        if (is_array($addresses)) {
+            foreach ($addresses as $address) {
+                $this->message->addTo($address);
+            }
+        } else {
+            $this->message->setTo($addresses);
+        }
+        return $this->message;
     }
 
+    /**
+     * Set the subject of this message.
+     * @param string $subject
+     * @return \Swift_Message $message
+     */
     public function setSubject($subject)
     {
         return $this->message->setSubject($subject);
     }
 
+    /**
+     * Set the body of this message.
+     * @param string $body
+     * @return \Swift_Message $message
+     */
     public function setBody($body)
     {
         return $this->message->setBody($body);
     }
 
     /**
-     * 
-     * @param string $view
-     * @param type $params
-     * @return string
+     * Renders a view file.
+     * @param string $view the view file.
+     * @param array $params the parameters (name-value pairs) that will be extracted and made available in the view file.
+     * @return string the rendering result
      */
     public function renderTemplate($view, $params = array())
     {
