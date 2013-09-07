0a5f9776ff6b5d2cce0061127980330446598d7a
Nghia Nguyen <zinzinday@gmail.com>
2013-09-08 04:49:21 +0700

    Fix php doc

--- yii/mailer/Mailer.php
+++ yii/mailer/Mailer.php
@@ -4,8 +4,6 @@
  * @link http://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
- * @author Nghia Nguyen <yiidevelop@hotmail.com>
- * @since 2.0
  */
 
 namespace yii\mailer;
@@ -19,6 +17,8 @@ use Yii;
  * @property \Swift_Message $message Message (RFC 2822) object.
  * @property \Swift_Transport $transport The Transport used to send messages.
  * @property mixed $failedRecipients Failures by-reference
+ * @author Nghia Nguyen <yiidevelop@hotmail.com>
+ * @since 2.0
  */
 class Mailer extends \yii\base\Object
 {
